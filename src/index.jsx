bfcf32e985c6acb0e0282870384d938aa9d713ca
Loreina Chew <loreina.chew@gmail.com>
2020-07-18 14:31:22 -0400

    style: add Heading65

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
 // Components
+export {default as Heading65} from './components/Heading68'
 export {default as Heading68} from './components/Heading68'
 export {default as Heading80} from './components/Heading80'
