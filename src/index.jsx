213437f7fa7f4c9644d12c35d1b92d9a6f193d0e
Loreina Chew <loreina.chew@gmail.com>
2020-07-18 15:59:39 -0400

    style: add Heading40

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 // Components
+export {default as Heading40} from './components/Heading40'
 export {default as Heading50} from './components/Heading50'
 export {default as Heading65} from './components/Heading68'
 export {default as Heading68} from './components/Heading68'
