ad2f8784e73a89482708ec6fbc9353c191917eeb
Loreina Chew <loreina.chew@gmail.com>
2020-07-18 18:20:37 -0400

    style: add typography elements (#9)

---
+++ src/components/Heading80.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading80 = styled.h1`
+  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
+  font-size: 80px;
+  font-weight: ${props => props.weight || 600};
+  opacity: ${props => props.opacity || 1};
+  color: ${props => props.color};
+`
+
+export default Heading80;
