31c62d11a64d062a322f90bba0d75394f6ec349b
Loreina Chew <loreina.chew@gmail.com>
2020-07-18 18:20:37 -0400

    style: add typography elements (#9)

---
+++ src/components/Heading40.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading40 = styled.h2`
+  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
+  font-size: 40px;
+  font-weight: ${props => props.weight || 600};
+  opacity: ${props => props.opacity || 1};
+  color: ${props => props.color};
+`
+
+export default Heading40;
