d17a25cb33fb53dcaf1e1850ed326bc421905216
Loreina Chew <loreina.chew@gmail.com>
2020-07-18 16:26:14 -0400

    style: add Heading25

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 // Components
-export {default as Heading30} from './components/Heading38'
-export {default as Heading35} from './components/Heading38'
+export {default as Heading25} from './components/Heading25'
+export {default as Heading30} from './components/Heading30'
+export {default as Heading35} from './components/Heading35'
 export {default as Heading38} from './components/Heading38'
 export {default as Heading40} from './components/Heading40'
 export {default as Heading50} from './components/Heading50'
