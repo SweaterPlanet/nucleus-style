400f9aa2d7b1db9d6b97250eb287fa50a42513be
Loreina Chew <loreina.chew@gmail.com>
2020-07-19 00:21:35 -0400

    style: add button components (#12)

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  /* text */
+  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
+  font-size: 40px;
+  font-weight: 600;
+
+  /* button styles */
+  border: none;
+  border-radius: 8px;
+  padding: 14px 38px;
+  color: ${props => props.color};
+  opacity: ${props => props.opacity || 1};
+  background-color: ${props => props.bg};
+  cursor: pointer;
+`
+
+export default Button;
