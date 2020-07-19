6da356b99e8a8ca7b78c3f9b696ada82bda0429a
Loreina Chew <loreina.chew@gmail.com>
2020-07-19 18:47:40 -0400

    perf: format code (#16)

--- src/components/Heading23.jsx
+++ src/components/Heading23.jsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Heading23 = styled.h3`
-  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
+  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
   font-size: 23px;
   font-weight: ${props => props.weight || 600};
   opacity: ${props => props.opacity || 1};
   color: ${props => props.color};
 `
 
-export default Heading23;
+export default Heading23
--- src/components/Heading35.jsx
+++ src/components/Heading35.jsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Heading35 = styled.h2`
-  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
+  font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
   font-size: 35px;
   font-weight: ${props => props.weight || 600};
   opacity: ${props => props.opacity || 1};
   color: ${props => props.color};
 `
 
-export default Heading35;
+export default Heading35
