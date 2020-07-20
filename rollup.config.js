a876a8ac255fdfc8cdcb233b16b7b2b41709e27c
Loreina Chew <loreina.chew@gmail.com>
2020-07-20 19:53:59 -0400

    build: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import filesize from 'rollup-plugin-filesize'
+
+import pkg from './package.json'
+
+const INPUT_FILE_PATH = 'src/index.js'
+
+const GLOBALS = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'prop-types': 'PropTypes',
+}
+
+const PLUGINS = [
+  resolve({
+    browser: true,
+    resolveOnly: [/^(?!react$)/, /^(?!react-dom$)/, /^(?!prop-types)/],
+  }),
+  commonjs(),
+  filesize(),
+]
+
+const EXTERNAL = ['react', 'react-dom', 'prop-types']
+
+const OUTPUT_DATA = [
+  {
+    file: pkg.browser,
+    format: 'umd',
+  },
+  {
+    file: pkg.main,
+    format: 'cjs',
+  },
+  {
+    file: pkg.module,
+    format: 'es',
+  },
+]
+
+const config = OUTPUT_DATA.map(({ file, format }) => ({
+  input: INPUT_FILE_PATH,
+  output: {
+    file,
+    format,
+    globals: GLOBALS,
+  },
+  external: EXTERNAL,
+  plugins: PLUGINS,
+}))
+
+export default config
