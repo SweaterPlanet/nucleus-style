cb688b699c491ded055ee36c4e91109dc5ae0718
Loreina Chew <loreina.chew@gmail.com>
2020-07-21 14:25:09 -0400

    fix: allow any fontSize in Link and LinkButton components

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Link = styled.a`
   font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif;
-  font-size: 20px;
+  font-size: ${props => props.fontSize || 20}px;
   font-weight: 600;
   opacity: ${props => props.opacity || 1};
   color: ${props => props.color || 'black'};
