d28ba03769f7da95f85c5903283c17ec063a9c7e
Loreina Chew <loreina.chew@gmail.com>
2020-07-24 16:45:04 -0400

    feat: add props to Flex component (#28)

--- src/components/Flex.jsx
+++ src/components/Flex.jsx
@@ -3,6 +3,21 @@ import Box from './Box'
 
 const Flex = styled(Box)`
   display: flex;
+
+  /* flex */
+  align-content: ${props => props.alignContent};
+  align-items: ${props => props.alignItems};
+  align-self: ${props => props.alignSelf};
+  flex: ${props => props.flex};
+  flex-basis: ${props => props.flexBasis};
+  flex-direction: ${props => props.flexDirection};
+  flex-grow: ${props => props.flexGrow};
+  flex-shrink: ${props => props.flexShrink};
+  flex-wrap: ${props => props.flexWrap};
+  justify-content: ${props => props.justifyContent};
+  justify-items: ${props => props.justifyItems};
+  justify-self: ${props => props.justifySelf};
+  order: ${props => props.order};
 `
 
 export default Flex
