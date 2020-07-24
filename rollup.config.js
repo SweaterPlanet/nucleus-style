9d65b5d3014ff89bb5811ea75a1c7363c4fa2473
Loreina Chew <loreina.chew@gmail.com>
2020-07-24 00:43:23 -0400

    build: fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,43 @@
-import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import pde from 'rollup-plugin-peer-deps-external'
 import filesize from 'rollup-plugin-filesize'
+import resolve from '@rollup/plugin-node-resolve'
 
-const formats = ['cjs', 'esm', 'umd']
+import pkg from './package.json'
 
-const plugins = [
-  resolve({
-    extensions: ['.js', '.jsx'],
-    resolveOnly: [/^(?!react$)/, /^(?!react-dom$)/, /^(?!styled-components)/],
-  }),
-  commonjs(),
-  filesize(),
-]
-
-export default [
-  {
-    input: 'src/index.js',
-    external: ['react', 'react-dom', 'styled-components'],
-    output: formats.map(format => ({
-      file: `./dist/index.${format}.js`,
-      format,
-      sourcemap: true,
-      name: 'nucleus-style',
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
       globals: {
         react: 'React',
         'react-dom': 'ReactDOM',
         'styled-components': 'styled',
       },
-    })),
-    plugins,
-  },
-]
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'styled-components': 'styled',
+      },
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    pde(),
+    resolve({
+      extensions: ['.js', '.jsx'],
+      resolveOnly: [/^(?!react$)/, /^(?!react-dom$)/, /^(?!styled-components)/],
+    }),
+    commonjs(),
+    filesize(),
+  ],
+}
