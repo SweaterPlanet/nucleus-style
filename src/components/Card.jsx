204b8e66dc4026a5e54eb1b3ed325d5419f26884
Loreina Chew <loreina.chew@gmail.com>
2020-07-26 17:15:01 -0400

    feat: add more card props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,11 @@ import Box from './Box'
 
 const Card = styled(Box)`
   border-radius: ${props => (props.rounded ? '8px' : 0)};
+  outline: ${props => props.outline || ''};
+  opacity: ${props => props.opacity || 1};
+  padding: ${props => props.padding || props.p || '20px'};
+
+  /* TODO: opacity, border outline, default padding */
 `
 
 export default Card
