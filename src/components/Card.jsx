03b1c528d0a03d95d894e1e1771e8a50eff228e5
Loreina Chew <loreina.chew@gmail.com>
2020-07-26 04:40:16 -0400

    feat: add rounded prop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import Box from './Box'
 
 const Card = styled(Box)`
-  border-radius: ${props => props.borderRadius};
+  border-radius: ${props => (props.rounded ? '8px' : 0)};
 `
 
 export default Card
