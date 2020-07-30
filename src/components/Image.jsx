b8928be92f528f66ad1614f023b7618dc59f500e
loreina <loreina.chew@gmail.com>
2020-07-29 23:05:33 -0400

    feat: add Image variants

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,26 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import Button from './Button'
 
-const Image = styled.img.attrs(props => ({
-  height: props.size,
-  width: props.size,
-  alt: props.alt,
-}))
+const StyledImage = styled(Button)`
+  border-radius: 0;
+
+  ${props =>
+    props.rounded &&
+    css`
+      border-radius: 8px;
+    `}
+
+  ${props =>
+    props.circle &&
+    css`
+      border-radius: 50%;
+      width: ${props => props.size};
+      height: ${props => props.size};
+    `}
+`
+
+const Image = ({ src, ...props }) => (
+  <StyledImage as="img" src={src} {...props} />
+)
 
 export default Image
