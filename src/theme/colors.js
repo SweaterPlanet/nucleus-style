8e1ab2c5ecb117d003e5de074e1e9ec614e448be
Loreina Chew <loreina.chew@gmail.com>
2020-08-01 03:55:06 -0400

    style: add theming (#40)

---
+++ src/theme/colors.js
@@ -0,0 +1,73 @@
+const aqua = {
+  lighter: '#CEF2FD',
+  light: '#9DE5FB',
+  base: '#3BCAF7',
+  dark: '#0897C4',
+  darker: '#044C62',
+}
+
+const black = '#000000'
+
+const blue = {
+  lighter: '#D2E8F9',
+  light: '#A6D1F2',
+  base: '#4DA2E5',
+  dark: '#1A6FB2',
+  darker: '#0D3859',
+}
+
+const green = {
+  lighter: '#D6F5DF',
+  light: '#ADEBBE',
+  base: '#5ED47F',
+  dark: '#28A44B',
+  darker: '#145225',
+}
+
+const paper = {
+  lighter: white,
+  light: '#F1F2F4',
+  base: '#E2E6E9',
+  dark: '#C5CDD3',
+  darker: '#A8B4BD',
+}
+
+const purple = {
+  lighter: '#DDD7F4',
+  light: '#BAAEEA',
+  base: '#765ED4',
+  dark: '#432AA2',
+  darker: '#211551',
+}
+
+const red = {
+  lighter: '#F7D4D4',
+  light: '#F3A5A5',
+  base: '#E05252',
+  dark: '#B01C1C',
+  darker: '#580E0E',
+}
+
+const slate = {
+  lighter: '#4B5C67',
+  light: '#36414A',
+  base: '#21272C',
+  dark: '#0F1215',
+  darker: black,
+}
+
+const white = '#ffffff'
+
+const colors = {
+  aqua,
+  black,
+  blue,
+  green,
+  paper,
+  purple,
+  red,
+  slate,
+  white,
+}
+
+export default colors
