957118f53c1bd3edd8d9e5d58a1b2785a6468d5a
loreina <loreina.chew@gmail.com>
2020-08-01 15:01:34 -0400

    style: add fonts theming

---
+++ src/theme/fonts.js
@@ -0,0 +1,8 @@
+const fonts = {
+  body:
+    "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
+  header:
+    "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
+}
+
+export default fonts
