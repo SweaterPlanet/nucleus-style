ba32913295dbd70584b8707cf0cb25370c3a78a0
loreina <loreina.chew@gmail.com>
2020-08-01 03:25:10 -0400

    feat: add root theme file

---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+// Color palette
+import { default as colors } from './colors'
+
+// Screen breakpoints
+import { default as screens } from './screens'
+
+export default {
+  colors,
+  screens,
+}
