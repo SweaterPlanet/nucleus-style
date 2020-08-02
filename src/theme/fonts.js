1b4275113ff38056a5b00cefc701856e7b59734d
loreina <loreina.chew@gmail.com>
2020-08-02 18:36:19 -0400

    feat: new Button component

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -5,4 +5,42 @@ const fonts = {
     "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
 }
 
-export default fonts
+const fontSizes = {
+  h1: {
+    xs: '35px',
+    sm: '40px',
+    md: '50px',
+    lg: '65px',
+    xl: '80px',
+  },
+  h2: {
+    xs: '22px',
+    sm: '25px',
+    md: '30px',
+    lg: '40px',
+    xl: '50px',
+  },
+  h3: {
+    xs: '16px',
+    sm: '18px',
+    md: '20px',
+    lg: '25px',
+    xl: '30px',
+  },
+  h4: {
+    xs: '13px',
+    sm: '14px',
+    md: '15px',
+    lg: '18px',
+    xl: '22px',
+  },
+  p: {
+    xs: '12px',
+    sm: '13px',
+    md: '13px',
+    lg: '15px',
+    xl: '18px',
+  },
+}
+
+export { fonts, fontSizes }
