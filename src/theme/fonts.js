b6736a3d1d77d6398e77819e3ffec307d4698368
loreina <loreina.chew@gmail.com>
2020-08-02 15:10:09 -0400

    fix: reimport body fonts

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -1,6 +1,6 @@
 const fonts = {
   body:
-    "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
+    "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
   header:
     "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
 }
