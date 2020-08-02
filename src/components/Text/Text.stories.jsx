0bc6a1e6ee48786cb942d918551a8a8c1762dc8f
loreina <loreina.chew@gmail.com>
2020-08-02 15:12:37 -0400

    docs: add knobs for Text

--- src/components/Text/Text.stories.jsx
+++ src/components/Text/Text.stories.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { withKnobs, boolean } from '@storybook/addon-knobs/react'
+import {
+  withKnobs,
+  boolean,
+  number,
+  select,
+  text,
+} from '@storybook/addon-knobs/react'
 import Text from './index'
 
 export default {
@@ -8,4 +14,35 @@ export default {
   decorators: [withKnobs],
 }
 
-export const Default = () => <Text bold={boolean('Bold', false)}>Text</Text>
+const boldLabel = 'Bold'
+const boldDefault = false
+
+const textLabel = 'Text'
+const textDefault = 'Text'
+
+const colorLabel = 'Color'
+const colorOptions = {
+  aqua: 'aqua',
+  black: 'black',
+  blue: 'blue',
+  green: 'green',
+  paper: 'paper',
+  purple: 'purple',
+  red: 'red',
+  slate: 'slate',
+  white: 'white',
+}
+const colorDefault = 'black'
+
+const opacityLabel = 'Opacity'
+const opacityDefault = '1'
+
+export const Default = () => (
+  <Text
+    bold={boolean(boldLabel, boldDefault)}
+    color={select(colorLabel, colorOptions, colorDefault)}
+    opacity={number(opacityLabel, opacityDefault)}
+  >
+    {text(textLabel, textDefault)}
+  </Text>
+)
