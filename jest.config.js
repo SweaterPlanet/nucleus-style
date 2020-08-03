dd4d0f7028ce883fd9955c0f46c339fc50732663
loreina <loreina.chew@gmail.com>
2020-08-03 00:36:10 -0400

    test: resolve snapshots path

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-{
-  "testRegex": "((\\.|/*.)(spec))\\.js?$",
-  "setupFilesAfterEnv": [
-    "./.jest/jest.setup.js"
-  ]
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testRegex: '((\\.|/*.)(test))\\.(js|jsx)?$',
+  setupFilesAfterEnv: ['./.jest/jest.setup.js'],
+  snapshotResolver: './.jest/snapshotResolver.js',
 }
