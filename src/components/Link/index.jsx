dc10ad977e91793622a8751434e4aa26cf4a3f1f
loreina <loreina.chew@gmail.com>
2020-08-07 01:30:37 -0400

    style: add size prop

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -12,6 +12,116 @@ const StyledLink = styled.a`
   background: none;
   border: none;
   cursor: pointer;
+
+  ${props =>
+    props.size === 'xs' &&
+    `
+    font-size: ${theme.fontSizes.p.xs}
+
+    @media (min-width: ${theme.screens.sm}) {
+      font-size: ${theme.fontSizes.p.sm}
+    }
+
+    @media (min-width: ${theme.screens.md}) {
+      font-size: ${theme.fontSizes.p.md}
+    }
+
+    @media (min-width: ${theme.screens.lg}) {
+      font-size: ${theme.fontSizes.p.lg}
+    }
+
+    @media (min-width: ${theme.screens.xl}) {
+      font-size: ${theme.fontSizes.p.xl}
+    }
+  `}
+
+  ${props =>
+    props.size === 'sm' &&
+    `
+    font-size: ${theme.fontSizes.h4.xs}
+
+    @media (min-width: ${theme.screens.sm}) {
+      font-size: ${theme.fontSizes.h4.sm}
+    }
+
+    @media (min-width: ${theme.screens.md}) {
+      font-size: ${theme.fontSizes.h4.md}
+    }
+
+    @media (min-width: ${theme.screens.lg}) {
+      font-size: ${theme.fontSizes.h4.lg}
+    }
+
+    @media (min-width: ${theme.screens.xl}) {
+      font-size: ${theme.fontSizes.h4.xl}
+    }
+  `}
+
+  ${props =>
+    props.size === 'md' &&
+    `
+    font-size: ${theme.fontSizes.h3.xs}
+
+    @media (min-width: ${theme.screens.sm}) {
+      font-size: ${theme.fontSizes.h3.sm}
+    }
+
+    @media (min-width: ${theme.screens.md}) {
+      font-size: ${theme.fontSizes.h3.md}
+    }
+
+    @media (min-width: ${theme.screens.lg}) {
+      font-size: ${theme.fontSizes.h3.lg}
+    }
+
+    @media (min-width: ${theme.screens.xl}) {
+      font-size: ${theme.fontSizes.h3.xl}
+    }
+  `}
+
+  ${props =>
+    props.size === 'lg' &&
+    `
+    font-size: ${theme.fontSizes.h2.xs}
+
+    @media (min-width: ${theme.screens.sm}) {
+      font-size: ${theme.fontSizes.h2.sm}
+    }
+
+    @media (min-width: ${theme.screens.md}) {
+      font-size: ${theme.fontSizes.h2.md}
+    }
+
+    @media (min-width: ${theme.screens.lg}) {
+      font-size: ${theme.fontSizes.h2.lg}
+    }
+
+    @media (min-width: ${theme.screens.xl}) {
+      font-size: ${theme.fontSizes.h2.xl}
+    }
+  `}
+
+  ${props =>
+    props.size === 'xl' &&
+    `
+    font-size: ${theme.fontSizes.h1.xs}
+
+    @media (min-width: ${theme.screens.sm}) {
+      font-size: ${theme.fontSizes.h1.sm}
+    }
+
+    @media (min-width: ${theme.screens.md}) {
+      font-size: ${theme.fontSizes.h1.md}
+    }
+
+    @media (min-width: ${theme.screens.lg}) {
+      font-size: ${theme.fontSizes.h1.lg}
+    }
+
+    @media (min-width: ${theme.screens.xl}) {
+      font-size: ${theme.fontSizes.h1.xl}
+    }
+  `}
 `
 
 function Link(props) {
