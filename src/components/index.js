72e06fbf52e6163b3728eac5f2bb5a7fc9ac2924
Loreina Chew <loreina.chew@gmail.com>
2020-08-13 01:15:03 -0400

    chore: reorganize components folder (#75)

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// Components (in alphabetical order)
+export { default as Avatar } from './Avatar'
+export { default as Box } from './Box'
+export { default as Button } from './Button'
+export { default as Card } from './Card'
+export { default as Flex } from './Flex'
+export { default as Heading } from './Heading'
+export { default as Image } from './Image'
+export { default as Link } from './Link'
+export { default as Tag } from './Tag'
+export { default as Text } from './Text'
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 // Components
-export { default as Avatar } from './components/Avatar'
-export { default as Box } from './components/Box'
-export { default as Button } from './components/Button'
-export { default as Card } from './components/Card'
-export { default as Flex } from './components/Flex'
-export { default as Heading } from './components/Heading'
-export { default as Image } from './components/Image'
-export { default as Link } from './components/Link'
-export { default as Tag } from './components/Tag'
-export { default as Text } from './components/Text'
+export * from './components'
 
 // Theme
-export { default as theme } from './theme'
+export * from './theme'
