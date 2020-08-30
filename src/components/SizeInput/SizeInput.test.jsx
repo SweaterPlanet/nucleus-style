328790507ba334e2995a1cc5ed9496d6e7d9f7e2
loreina <loreina.chew@gmail.com>
2020-08-30 17:30:02 -0400

    test: add SizeInput tests

---
+++ src/components/SizeInput/SizeInput.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+
+import SizeInput from '.'
+
+describe('SizeInput', () => {
+  it('renders correctly', () => {
+    const test = shallow(<SizeInput />)
+    expect(test).toMatchSnapshot()
+  })
+
+  it('respects the dark prop', () => {
+    const testProp = true
+    const test = mount(<SizeInput dark={testProp} />)
+    expect(test.prop('dark')).toBe(true)
+  })
+
+  it('respects the label prop', () => {
+    const testProp = 'xs'
+    const test = mount(<SizeInput label={testProp} />)
+    expect(test.prop('label')).toBe(testProp)
+  })
+
+  it('respects the placeholder prop', () => {
+    const testProp = 3
+    const test = mount(<SizeInput placeholder={testProp} />)
+    expect(test.prop('placeholder')).toBe(testProp)
+  })
+
+  it('respects the red prop', () => {
+    const testProp = true
+    const test = mount(<SizeInput red={testProp} />)
+    expect(test.prop('red')).toBe(true)
+  })
+
+  it('respects the stock prop', () => {
+    const testProp = 4
+    const test = mount(<SizeInput stock={testProp} />)
+    expect(test.prop('stock')).toBe(testProp)
+  })
+})
