7c4edf7376a690adec4bfed1d28cca59a654c0a8
loreina <loreina.chew@gmail.com>
2020-08-30 16:23:41 -0400

    feat: add prop types

--- src/components/SizeInput/index.jsx
+++ src/components/SizeInput/index.jsx
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import theme from '../../theme'
 
+import Box from '../Box'
 import Flex from '../Flex'
+import Text from '../Text'
 
-const Box = styled(Flex)`
+const Wrapper = styled(Flex)`
+  float: left;
+  margin-right: 10px;
+`
+
+const InputBox = styled(Flex)`
   width: 72px;
   height: 36px;
   background-color: ${theme.colors.paper.dark};
   margin: 0 auto;
-  position: absolute;
+  position: relative;
   direction: row;
 `
 
@@ -22,6 +29,7 @@ const Label = styled.p`
   padding-left: 12px;
   color: ${theme.colors.slate.base};
   text-align: left;
+  text-transform: uppercase;
 `
 
 const Input = styled.input`
@@ -36,7 +44,7 @@ const Input = styled.input`
   width: 20px;
   height: 16px;
   margin: 8px;
-  margin-left: 8px;
+  margin-right: 8px;
 
   /* Hide number input errors */
   -moz-appearance: textfield;
@@ -78,13 +86,59 @@ const Input = styled.input`
   }
 `
 
+const Caption = styled.p`
+  font-family: ${theme.fonts.body};
+  font-size: 12px;
+  color: ${theme.colors.slate.base};
+  text-align: left;
+  margin-top: 4px;
+
+  ${props =>
+    props.red &&
+    `
+      color: ${theme.colors.red.base};
+    `}
+`
+
 function SizeInput(props) {
+  if (props.stock) {
+    return (
+      <>
+        <Wrapper flexDirection="column">
+          <InputBox>
+            <Label bold>{props.label}</Label>
+            <Input type="number" placeholder={props.placeholder} />
+          </InputBox>
+          <Box>
+            <Caption red={props.red}>{props.stock} in stock</Caption>
+          </Box>
+        </Wrapper>
+      </>
+    )
+  }
+
   return (
-    <Box>
-      <Label bold>XS</Label>
-      <Input type="number" placeholder="0" />
-    </Box>
+    <>
+      <Wrapper flexDirection="column">
+        <InputBox>
+          <Label bold>{props.label}</Label>
+          <Input type="number" placeholder={props.placeholder} />
+        </InputBox>
+      </Wrapper>
+    </>
   )
+}
+
+SizeInput.propTypes = {
+  label: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  placeholder: PropTypes.number,
+  red: PropTypes.bool,
+  stock: PropTypes.number,
+}
+
+SizeInput.defaultProps = {
+  placeholder: 0,
+  red: false,
 }
 
 export default SizeInput
