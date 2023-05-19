8ec7cc6ff10beaa4651bed1ce25d19579563ad44
DexterStorey <dexter@dexterstorey.com>
2023-05-19 12:19:59 -0400

    yolo

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -1,5 +1,6 @@
 const fonts = {
-  body: "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
+  body:
+    "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
   header:
     "'Helvetica', -apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif",
 }
